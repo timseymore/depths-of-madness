4392c5506f578349af52a5efed0931334683cd44
Tim Seymore <tim_seymore2012@hotmail.com>
2020-01-31 20:14:01 -0500

    Create Game class

--- src/models/enviroment/door.py
+++ src/models/enviroment/door.py
@@ -6,13 +6,12 @@ from src.ui.tools.colors import Color
 class Door(pygame.sprite.Sprite):
     """ The door that clears the current level."""
 
-    text = "A door that exits the level."
-
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, exit_level):
         """ Constructor Method """
 
         super().__init__()
-
+        self.exit_level = exit_level
+        self.text = "A door that exits to level " + str(self.exit_level)
         self.image = pygame.Surface([width, height])
         self.image.fill(Color.DarkBrown)
         self.rect = pygame.rect.Rect((x, y), self.image.get_size())
