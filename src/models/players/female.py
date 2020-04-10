8e616710958cbd23fc834ff31470622e3ee4c764
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-10 18:26:28 -0400

    Refactor player sub classes

--- src/models/players/female.py
+++ src/models/players/female.py
@@ -4,30 +4,28 @@ from src.models.players.player import Player
 
 
 class Female(Player):
-    """
-     Female is a Player(Integer[0, WIDTH], Integer[0, HEIGHT])
+    """ Female is a Player(Integer[0, WIDTH], Integer[0, HEIGHT])
+
      interp. f = Female(x, y) creates a playable female character where:
               - x is the x coordinate
               - y is the y coordinate
-     f = Female(20, 10)  creates a female player at x position 20 and y position 10
-
-     def fn-for-female(f):      Female
-         ... f.x                Integer[0, WIDTH]
-             f.y                Integer[0, HEIGHT]
      """
+
     def __init__(self, x, y):
+        """ Constructor method
+
+        - x: int :  x location to spawn
+        - y: int :  y location to spawn
         """
-        Constructor method
-        :param x: x location to spawn
-        :param y: y location to spawn
-        """
+        
         super().__init__(x, y)
         self.image = pygame.image.load(r'src/graphics/female_right.png')
         self.rect = pygame.rect.Rect((x, y), self.image.get_size())
-        self.gender = "female"
+        self.gender = 'female'
 
     def switch_img(self, last, new):
         """ Switches img based on direction of movement. """
+
         if last.right < new.right:
             self.image = pygame.image.load(r'src/graphics/female_right.png')
         elif last.left > new.left:
