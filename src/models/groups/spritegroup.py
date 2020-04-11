cfee6eaf51c914ac6c187f833a33291d6dda4fa9
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-11 18:20:58 -0400

    Update spritegroup.py

--- src/models/groups/spritegroup.py
+++ src/models/groups/spritegroup.py
@@ -2,5 +2,10 @@ import pygame
 
 
 class SpriteGroup(pygame.sprite.Group):
+    """ A sprite group list used in game
+
+    used to improve readability and possible changes in implementation
+    """
+
     def __int__(self):
         super().__init__()
